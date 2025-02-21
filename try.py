4c5eef59be50b2edf71c9a2212f061ce9252e6e4
britto-772004 <michaeljohnbritto772004@gmail.com>
2025-02-21 17:13:04 +0530

    before hackathon

--- try.py
+++ try.py
@@ -3,9 +3,13 @@ from gpiozero import LED
 import app
 import time
 # Define LEDs
-red = LED(4)
-yellow = LED(6)
-green = LED(2)
+red_1 = LED(4) # 4th pin 
+yellow_1 = LED(6) # keela iruthu 5th pin 
+green_1 = LED(2) #2nd pin
+
+red_2 = LED(17) # 6th pin from the up side
+yellow_2 = LED(27) #7th pin from the up side
+green_2 = LED(22) #8th  pin from the up side
 
 # def light():
 #     try:
@@ -45,24 +49,44 @@ def funtime():
     return ans
 
 while True:
-    red.on()
+    red_1.on()
     print("Red light is on....")
     sleep(5)
-    red.off()
+    red_1.off()
     print("Red light is off....")
-    yellow.on()
+    yellow_1.on()
     print("yellow light is on....")
     ans=funtime()
     # sleep(3)
     sleep(1)
-    yellow.off()
+    yellow_1.off()
     print("yellow light is off....")
     # ans=time()
-    green.on()
+    green_1.on()
     print("Green light is on....")
     sleep(ans-25)
-    green.off()
+    green_1.off()
     print("Green light is off....")
+
+    red_2.on()
+    print("Red light is on....")
+    sleep(5)
+    red_2.off()
+    print("Red light is off....")
+    yellow_2.on()
+    print("yellow light is on....")
+    ans=funtime()
+    # sleep(3)
+    sleep(1)
+    yellow_2.off()
+    print("yellow light is off....")
+    # ans=time()
+    green_2.on()
+    print("Green light is on....")
+    sleep(ans-25)
+    green_2.off()
+    print("Green light is off....")
+
 
 """
 to find excution time
@@ -71,3 +95,22 @@ ans=funtime()
 endtime=time.time()
 print("ex time",endtime-startTime)
 """
+
+# while True:
+#     red_2.on()
+#     print("red - 2 in on....")
+#     green_1.on()
+#     print("green - 1 is on....")
+#     sleep(5)
+#     green_1.off()
+#     print("green - 1 is off")
+#     yellow_1.on()
+#     print("yellow - 1 is on...")
+#     ans = funtime()
+#     sleep(1)
+#     yellow_1.off()
+#     print("yellow -1 is off...")
+#     red_2.off()
+#     print("red - 2 is off..")
+#     red_1.on()
+#     print("red - 1 is on..")
